ad9e48a07134a71e10821b028aa076307081acd7
github username <github email address>
2018-06-12 21:44:17 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,41 @@
+require 'pry'
+
+
 class Pokemon
+
+  attr_reader :id, :name, :type, :db
+
+  def initialize(props={})
+    @id = props['id']
+    @name = props['name']
+    @type = props['type']
+    @db = props['db']
+  end
+
+  def self.save(name, type, db)
+
+    sql_string = <<-HELLO
+      insert into pokemon
+      (name, type)
+      values
+      (?, ?)
+    HELLO
+
+    db.execute(sql_string, name, type)
+  end
+
+  def self.find(id, db)
+    sql_string = <<-PIKA
+    select *
+    from pokemon
+    where ID = ?
+    PIKA
+
+    array = db.execute(sql_string, id)
+    hash = {"name" => array[0][1], "id" => array[0][0], "type" => array[0][2], "hp" => 60}
+    Pokemon.new(hash)
+
+  end
+
+
 end
